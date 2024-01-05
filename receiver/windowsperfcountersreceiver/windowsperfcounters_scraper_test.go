eca967c53e209ea8f9e41260002b903705ef815c
pjanotti <pjanotti@splunk.com>
2024-01-05 10:15:54 -0800

    [chore][receiver/windowsperfcounters] Fix lint on Windows

--- receiver/windowsperfcountersreceiver/windowsperfcounters_scraper_test.go
+++ receiver/windowsperfcountersreceiver/windowsperfcounters_scraper_test.go
@@ -29,7 +29,6 @@ import (
 
 type mockPerfCounter struct {
 	counterValues []winperfcounters.CounterValue
-	metricRep     MetricRep
 	path          string
 	scrapeErr     error
 	closeErr      error
@@ -58,9 +57,8 @@ func Test_WindowsPerfCounterScraper(t *testing.T) {
 		name string
 		cfg  *Config
 
-		mockCounterPath string
-		startMessage    string
-		startErr        string
+		startMessage string
+		startErr     string
 
 		expectedMetricPath string
 	}
@@ -176,7 +174,8 @@ func Test_WindowsPerfCounterScraper(t *testing.T) {
 
 			require.NoError(t, err)
 			expectedMetrics, err := golden.ReadMetrics(test.expectedMetricPath)
-			pmetrictest.CompareMetrics(expectedMetrics, actualMetrics, pmetrictest.IgnoreMetricValues())
+			require.NoError(t, err)
+			err = pmetrictest.CompareMetrics(expectedMetrics, actualMetrics, pmetrictest.IgnoreMetricValues())
 			require.NoError(t, err)
 		})
 	}
@@ -341,7 +340,8 @@ func TestScrape(t *testing.T) {
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
 			mpc := mockPerfCounter{counterValues: test.mockCounterValues}
-			s := &scraper{cfg: &test.cfg, newWatcher: mockPerfCounterFactory(mpc)}
+			testConfig := test.cfg
+			s := &scraper{cfg: &testConfig, newWatcher: mockPerfCounterFactory(mpc)}
 			errs := s.start(context.Background(), componenttest.NewNopHost())
 			require.NoError(t, errs)
 
